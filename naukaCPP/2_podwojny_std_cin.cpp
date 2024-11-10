90896a0db864cacf86ef9bc218c17226817a6354
Sztupson <148426917+Sztupson@users.noreply.github.com>
2024-11-10 15:08:28 +0100

    Pierwsze 8 lekcji

---
+++ naukaCPP/2_podwojny_std_cin.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {  
+    std::string name;
+    int age;
+
+    std::cout << "Podaj swoje imie i nazwisko: " << std::endl;
+    std::cin >> name >> age;
+}
---
+++ naukaCPP/8_loops__size_t.cpp
@@ -0,0 +1,7 @@
+ #include <iostream>
+
+int main() {  
+for(size_t i = 0; i < 10; i++){
+    std::cout << "I love cpp" << std::endl;
+}
+}
